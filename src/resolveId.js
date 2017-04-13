7e8c972a406a942740371536687464fab3e03207
Skyler Lipthay <sl@skylerlipthay.com>
2017-04-13 00:26:43 -0700

    Support `options.path` as an array

--- src/resolveId.js
+++ src/resolveId.js
@@ -1,7 +1,21 @@
 import { dirname, resolve } from 'path';
+import { existsSync } from 'fs';
 
 export default function resolveId(file, url, opts) {
     if (opts.path) {
+        if (Array.isArray(opts.path)) {
+            let selected = opts.path[0];
+
+            opts.path.forEach((path) => {
+                const current = existsSync(resolve(selected, url));
+                if (current) {
+                    selected = current;
+                }
+            });
+
+            return resolve(selected, url);
+        }
+
         return resolve(opts.path, url);
     }
     if (file) {
